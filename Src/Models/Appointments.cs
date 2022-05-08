daa9a0accc99f303d336baadf031433e7dc738d1
Dillikumaran <101106085+Dillikumaran@users.noreply.github.com>
2022-05-08 20:11:10 +0530

    Add files via upload

---
+++ Src/Models/Appointments.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectA.Models
+{
+    public class Appointments
+    {
+        public int AppointmentId { get; set; }
+        public int PatientId { get; set; }
+        public string Specialization { get; set; }
+        public string Doctor { get; set; }
+        public string VisitDate { get; set; }
+        public string AppointmentTime { get; set; }
+    }
+}
---
+++ Src/Models/Patient.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectA.Models
+{
+    public class Patient
+    {
+        public int PatientId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Sex { get; set; }
+        public int Age { get; set; }
+        public DateTime DateOfBirth { get; set;}
+    }
+}
